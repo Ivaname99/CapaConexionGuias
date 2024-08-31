c7c7e060f55e41e80e84d7b180b33ef4c993b804
IvElGrimm <126293620+Ivaname99@users.noreply.github.com>
2024-08-30 21:04:16 -0600

    Cambios estéticos

--- CapaConexion/Form1.Designer.cs
+++ CapaConexion/Form1.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.dataGrid = new System.Windows.Forms.DataGridView();
             this.btnCargar = new System.Windows.Forms.Button();
-            this.txtFiltro = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtBuscar = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
@@ -48,35 +46,18 @@
             // 
             // btnCargar
             // 
-            this.btnCargar.Location = new System.Drawing.Point(33, 387);
+            this.btnCargar.Location = new System.Drawing.Point(206, 378);
             this.btnCargar.Name = "btnCargar";
-            this.btnCargar.Size = new System.Drawing.Size(113, 23);
+            this.btnCargar.Size = new System.Drawing.Size(113, 43);
             this.btnCargar.TabIndex = 1;
             this.btnCargar.Text = "Cargar Informacion";
             this.btnCargar.UseVisualStyleBackColor = true;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
-            // txtFiltro
-            // 
-            this.txtFiltro.Location = new System.Drawing.Point(283, 389);
-            this.txtFiltro.Name = "txtFiltro";
-            this.txtFiltro.Size = new System.Drawing.Size(100, 20);
-            this.txtFiltro.TabIndex = 2;
-            this.txtFiltro.TextChanged += new System.EventHandler(this.txtFiltro_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(245, 391);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Filtrar";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(560, 44);
+            this.label2.Location = new System.Drawing.Point(546, 143);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 13);
             this.label2.TabIndex = 4;
@@ -84,16 +65,16 @@
             // 
             // txtBuscar
             // 
-            this.txtBuscar.Location = new System.Drawing.Point(636, 41);
+            this.txtBuscar.Location = new System.Drawing.Point(622, 140);
             this.txtBuscar.Name = "txtBuscar";
             this.txtBuscar.Size = new System.Drawing.Size(100, 20);
             this.txtBuscar.TabIndex = 5;
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(601, 81);
+            this.btnBuscar.Location = new System.Drawing.Point(579, 175);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(87, 23);
+            this.btnBuscar.Size = new System.Drawing.Size(87, 35);
             this.btnBuscar.TabIndex = 6;
             this.btnBuscar.Text = "Buscar Cliente";
             this.btnBuscar.UseVisualStyleBackColor = true;
@@ -103,12 +84,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(758, 450);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.txtBuscar);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtFiltro);
             this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.dataGrid);
             this.Name = "Form1";
@@ -124,8 +103,6 @@
 
         private System.Windows.Forms.DataGridView dataGrid;
         private System.Windows.Forms.Button btnCargar;
-        private System.Windows.Forms.TextBox txtFiltro;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtBuscar;
         private System.Windows.Forms.Button btnBuscar;
